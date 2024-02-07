b96d335141f5fe99ee7f804c3142e3eca7d3a47d
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-02-08 03:51:58 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -12,11 +12,11 @@ def solution(bandage, health, attacks):
             success += 1
             # 연속 성공이 시전 시간(time)보다 작은 경우
             if success < time:
-                # 최대 체력 이상인 경우
-                if answer + recover >= health:
-                    answer = health
-                else:
+                # 초당 회복량을 더한 값이 최대 체력보다 작은 경우
+                if answer + recover < health:
                     answer += recover
+                else:
+                    answer = health
             # 시전 시간(time) 연속 성공할 경우
             elif success == time:
                 success = 0
