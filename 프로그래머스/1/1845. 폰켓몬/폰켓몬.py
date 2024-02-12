24dfa689014519dc1d121da0190322aa154413ec
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-02-13 02:34:43 +0900

    [level 1] Title: 폰켓몬, Time: 1.27 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,5 @@
 def solution(nums):
-    answer = 0
     phonket = dict()
     for n in nums:
-        phonket[n] = phonket.get(n, 0) + 1
-    answer = min(len(nums)/2, len(phonket.keys()))
-    return answer
+        phonket[n] = phonket.get(n, 0) + 1 
+    return min(len(nums)/2, len(phonket.keys()))
