328e19b5a99707c2d879e0b992fec7cc0da32c28
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-02-13 12:08:28 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -7,14 +7,11 @@ def solution(progresses, speeds):
     while progresses:
         for i in range(len(speeds)):
             progresses[i] += speeds[i]
-        p = progresses[0]
-        if p >= 100:
-            cnt = 1
+        cnt = 0
+        while progresses and progresses[0] >= 100:
             progresses.popleft()
             speeds.popleft()
-            while progresses and progresses[0] >= 100:
-                progresses.popleft()
-                speeds.popleft()
-                cnt += 1
+            cnt += 1
+        if cnt > 0:
             answer.append(cnt)
     return answer
