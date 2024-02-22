77e92ec3c95683000ea9ea9a8cb99b7ef176aa89
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-02-22 13:54:00 +0900

    [level 2] Title: 호텔 대실, Time: 69.92 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -0,0 +1,27 @@
+def solution(book_time):
+    answer = 0
+    minutes = []
+    for start, end in book_time:
+        temp = []
+        start_hour, start_minute = start.split(':')
+        # print(start_hour, start_minute)
+        end_hour, end_minute = end.split(':')
+        # print(end_hour, end_minute)
+        start_hour = 60 * int(start_hour)
+        temp.append(start_hour+int(start_minute))
+        end_hour = 60 * int(end_hour)
+        temp.append(end_hour+int(end_minute))
+        minutes.append(temp)
+    minutes.sort(key=lambda x: x[0])
+    room = []
+    room.append(minutes[0])
+    answer += 1
+    for m in minutes[1:]:
+        if min(room, key=lambda x: x[1])[1]+10 > m[0]:
+            room.append(m)
+            answer += 1
+        else:
+            idx = room.index(min(room, key=lambda x: x[1]))
+            del room[idx]
+            room.append(m)
+    return answer
