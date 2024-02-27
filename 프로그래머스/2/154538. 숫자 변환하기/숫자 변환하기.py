3a5515f125f08c3bbd59c56ca1b13f52a4878e0c
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-02-27 15:47:35 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,38 +1,21 @@
-from collections import deque
-
 def solution(x, y, n):
     answer = 0
-    calc = [n, 2, 3]
-    visited = [0] * (y + 1)
-    queue = deque()
-    queue.append(x)
-    if x == y:
-        return 0
-    while queue:
-        start = queue.popleft()
-        for i in range(len(calc)):
-            if i == 0:
-                new_number = start + calc[i]
-                if start <= new_number <= y and visited[new_number] == 0:
-                    visited[new_number] = visited[start] + 1
-                    queue.append(new_number)
-                    if new_number == y:
-                        return visited[y]
-            elif i == 1:
-                new_number = start * calc[i]
-                if start <= new_number <= y and visited[new_number] == 0:
-                    visited[new_number] = visited[start] + 1
-                    queue.append(new_number)
-                    if new_number == y:
-                        return visited[y]
-            elif i == 2:
-                new_number = start * calc[i]       
-                if start <= new_number <= y and visited[new_number] == 0:
-                    visited[new_number] = visited[start] + 1
-                    queue.append(new_number)
-                    if new_number == y:
-                        return visited[y]
-    if visited[y] != 0:
-        return visited[y]
-    else:
-        return -1
+    dp = set()
+    dp.add(x)
+
+    while dp:
+        if y in dp:
+            return answer
+        else:
+            dp_y = set()
+            for i in dp:
+                if i+n <= y:
+                    dp_y.add(i+n)
+                if i*2 <= y:
+                    dp_y.add(i*2)
+                if i*3 <= y:
+                    dp_y.add(i*3)
+            dp = dp_y
+            answer += 1
+
+    return -1
