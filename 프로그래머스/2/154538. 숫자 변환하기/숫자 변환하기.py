257ea84f43c935267635fd23327892bd35bc2793
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-02-27 15:29:31 +0900

    [level 2] Title: 숫자 변환하기, Time: 6.57 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -6,13 +6,10 @@ def solution(x, y, n):
     visited = [0] * (y + 1)
     queue = deque()
     queue.append(x)
-    # visited[x] = 1
     if x == y:
         return 0
     while queue:
         start = queue.popleft()
-        # print(start)
-        # print(visited[start])
         for i in range(len(calc)):
             if i == 0:
                 new_number = start + calc[i]
@@ -20,7 +17,6 @@ def solution(x, y, n):
                     visited[new_number] = visited[start] + 1
                     queue.append(new_number)
                     if new_number == y:
-                        # print(visited)
                         return visited[y]
             elif i == 1:
                 new_number = start * calc[i]
@@ -28,7 +24,6 @@ def solution(x, y, n):
                     visited[new_number] = visited[start] + 1
                     queue.append(new_number)
                     if new_number == y:
-                        # print(visited)
                         return visited[y]
             elif i == 2:
                 new_number = start * calc[i]       
@@ -37,7 +32,6 @@ def solution(x, y, n):
                     queue.append(new_number)
                     if new_number == y:
                         return visited[y]
-    # print(visited)
     if visited[y] != 0:
         return visited[y]
     else:
