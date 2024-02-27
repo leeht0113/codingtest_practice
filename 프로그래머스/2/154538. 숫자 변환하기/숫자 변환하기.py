384fcab1159ddec16bf07952f295ad6420da6db6
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-02-27 15:50:18 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.70 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,3 +1,31 @@
+# from collections import deque
+
+# def solution(x, y, n):
+#     answer = 0
+#     visited = [0] * (y+1)
+#     move = [n, 2, 3]
+
+#     if x== y:
+#         return 0
+
+#     queue = deque()
+#     queue.append(x)
+#     while queue:
+#         start = queue.popleft()
+#         for i in range(len(move)):
+#             if i == 0:
+#                 loc = start + move[i]
+#             else:
+#                 loc = start * move[i]
+#             if loc <= y and visited[loc] == 0:
+#                 queue.append(loc)
+#                 visited[loc] = visited[start] + 1
+#     answer = visited[-1]
+#     if answer:
+#         return answer
+#     else:
+#         return -1
+
 def solution(x, y, n):
     answer = 0
     dp = set()
