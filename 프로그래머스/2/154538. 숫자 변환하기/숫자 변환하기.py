c51008583bd0b4a0fc20e6419e5819a9ccfc9426
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-02-27 15:28:48 +0900

    [level 2] Title: 숫자 변환하기, Time: 6.56 ms, Memory: 17.4 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -2,26 +2,43 @@ from collections import deque
 
 def solution(x, y, n):
     answer = 0
-    visited = [0] * (y+1)
-    move = [n, 2, 3]
-    
-    if x == y:
-        return 0
-    
+    calc = [n, 2, 3]
+    visited = [0] * (y + 1)
     queue = deque()
     queue.append(x)
+    # visited[x] = 1
+    if x == y:
+        return 0
     while queue:
         start = queue.popleft()
-        for i in range(len(move)):
+        # print(start)
+        # print(visited[start])
+        for i in range(len(calc)):
             if i == 0:
-                loc = start + move[i]
-            else:
-                loc = start * move[i]
-            if loc <= y and visited[loc] == 0:
-                queue.append(loc)
-                visited[loc] = visited[start] + 1
-    answer = visited[-1]
-    if answer:
-        return answer
+                new_number = start + calc[i]
+                if start <= new_number <= y and visited[new_number] == 0:
+                    visited[new_number] = visited[start] + 1
+                    queue.append(new_number)
+                    if new_number == y:
+                        # print(visited)
+                        return visited[y]
+            elif i == 1:
+                new_number = start * calc[i]
+                if start <= new_number <= y and visited[new_number] == 0:
+                    visited[new_number] = visited[start] + 1
+                    queue.append(new_number)
+                    if new_number == y:
+                        # print(visited)
+                        return visited[y]
+            elif i == 2:
+                new_number = start * calc[i]       
+                if start <= new_number <= y and visited[new_number] == 0:
+                    visited[new_number] = visited[start] + 1
+                    queue.append(new_number)
+                    if new_number == y:
+                        return visited[y]
+    # print(visited)
+    if visited[y] != 0:
+        return visited[y]
     else:
         return -1
