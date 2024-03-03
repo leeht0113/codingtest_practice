64109ee05d2b96b79191f5c2062aaa070c6898fc
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-03-03 18:53:02 +0900

    [level 2] Title: N-Queen, Time: 1117.90 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,4 +1,4 @@
-def getAns(n, y, width, diagonal1, diagonal2):
+def dfs(n, y, width, diagonal1, diagonal2):
     ans = 0
     if y == n:
         ans += 1
@@ -7,11 +7,11 @@ def getAns(n, y, width, diagonal1, diagonal2):
             if width[i] or diagonal1[i + y] or diagonal2[i - y + n]:
                 continue
             width[i] = diagonal1[i + y] = diagonal2[i - y + n] = True
-            ans += getAns(n, y + 1, width, diagonal1, diagonal2)
+            ans += dfs(n, y + 1, width, diagonal1, diagonal2)
             width[i] = diagonal1[i + y] = diagonal2[i - y + n] = False
     return ans
 
 def solution(n):
     # y 값은 현재 위치가 결정된 퀸의 개수를 의미
-    answer = getAns(n, 0, [False] * n, [False] * (n * 2), [False] * (n * 2))
+    answer = dfs(n, 0, [False] * n, [False] * (n * 2), [False] * (n * 2))
     return answer
