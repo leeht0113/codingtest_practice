a41bda245904263ceccf24d3433a72a541f3c7f1
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-03-03 12:33:00 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,13 +1,11 @@
 def dfs(cur_k, cnt, dungeons, visited):
-    answer_max = cnt
+    max_cnt = cnt
     for idx, d in enumerate(dungeons):
         if cur_k >= d[0] and visited[idx] == 0:
             visited[idx] = 1
-            answer_max = max(answer_max, dfs(cur_k - d[1], cnt + 1, dungeons, visited))
+            max_cnt = max(max_cnt, dfs(cur_k - d[1], cnt + 1, dungeons, visited))
             visited[idx] = 0
-    return answer_max
+    return max_cnt
 
 def solution(k, dungeons):
-    visited = [0] * len(dungeons)
-    answer_max = dfs(k, 0, dungeons, visited)
-    return answer_max
+    return dfs(k, 0, dungeons, [0] * len(dungeons))
