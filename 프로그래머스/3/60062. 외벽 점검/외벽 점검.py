12503d0ad3fb6287c26aa95ee59cdff79fd7615d
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-03-04 19:41:33 +0900

    [level 3] Title: 외벽 점검, Time: 363.10 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.py
@@ -0,0 +1,18 @@
+from itertools import permutations
+
+def solution(n, weak, dist):
+    length = len(weak)
+    weak.extend([w + n for w in weak])
+    answer = len(dist) + 1
+    for i in range(length):
+        for f in permutations(dist, len(dist)):
+            cnt = 1
+            position = weak[i] + f[cnt - 1]
+            for j in range(i, i + length):
+                if position < weak[j]:
+                    cnt += 1
+                    if cnt > len(dist):
+                        break
+                    position = weak[j] + f[cnt - 1]
+            answer = min(answer, cnt)
+    return answer if answer <= len(dist) else -1
