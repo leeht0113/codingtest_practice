2dfd859766624956e8f54b1dcb68138e071bcccf
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-03-08 12:15:07 +0900

    [level 3] Title: 사라지는 발판, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
@@ -1,38 +1,43 @@
+def is_valid_pos(x, y, board):
+    return 0 <= x < len(board) and 0 <= y < len(board[0])
+
+def dfs(aloc, bloc, visited, step, board):
+    if step % 2 == 0:
+        x, y = aloc
+    else:
+        x, y = bloc
+    can_move = False
+    is_opponent_winner = True
+    # 상대 플레이어가 이긴 경우의 이동횟수, 상대 플레이어가 진 경우의 이동횟수
+    win_steps, lose_steps = [], []
+    move_x = [-1, 1, 0, 0]
+    move_y = [0, 0, -1, 1]
+    for i in range(4):
+        new_x, new_y = x + move_x[i], y + move_y[i]
+        if is_valid_pos(new_x, new_y, board) and (new_x, new_y) not in visited and board[new_x][new_y]:
+            can_move = True
+            if aloc == bloc:
+                return True, step + 1
+            if step % 2 == 0:
+                win, steps_left = dfs([new_x, new_y], bloc, visited | {(x, y)}, step + 1, board)
+            else:
+                win, steps_left = dfs(aloc, [new_x, new_y], visited | {(x, y)}, step + 1, board)
+            # 한 번이라도 상대 플레이어가 지면 해당 값을 False로 만듦
+            is_opponent_winner &= win
+            if win:
+                win_steps.append(steps_left)
+            else:
+                lose_steps.append(steps_left)
+    # 현재 플레이어가 더 이상 이동할 수 없으면 현재 플레이어가 진 것과 같음
+    if not can_move:
+        return False, step
+    # 상대 플레이어가 이긴 경우 패배한 플레이어는 최선을 다하므로 win_steps의 최댓값을 반환
+    if is_opponent_winner:
+        return False, max(win_steps)
+    # 상대 플레이어가 이길 수도 있고 질 수도 있음, 현재 플레이어는 무조건 이기는 경우를 택함
+    # 가장 짧은 이동 횟수로 이겨야 하므로 lose_steps의 최솟값을 택함
+    return True, min(lose_steps)
+
 def solution(board, aloc, bloc):
-    answer = -1
-    ROW, COL = len(board), len(board[0])
-    DR, DC = [-1, 0, 1, 0], [0, 1, 0, -1]
-    
-    def is_valid_pos(r, c):
-        return 0 <= r < ROW and 0 <= c < COL
-    
-    def recursive_func(alpha_pos, beta_pos, visited, step):
-        r, c = alpha_pos if step % 2 == 0 else beta_pos
-        can_move = False
-        is_opponent_winner = True
-        win_steps, lose_steps = [], []
-        for i in range(4):
-            nr, nc = r + DR[i], c + DC[i]
-            if is_valid_pos(nr, nc) and (nr, nc) not in visited and board[nr][nc]:
-                can_move = True
-                if alpha_pos == beta_pos:
-                    return True, step + 1
-                
-                win, steps_left = (
-                    recursive_func([nr, nc], beta_pos, visited | {(r, c)}, step + 1)
-                    if step % 2 == 0
-                    else recursive_func(
-                        alpha_pos, [nr, nc], visited | {(r, c)}, step + 1
-                    )
-                )
-                is_opponent_winner &= win
-                (win_steps if win else lose_steps).append(steps_left)
-                
-        if not can_move:
-            return False, step
-        if is_opponent_winner:
-            return False, max(win_steps)
-        return True, min(lose_steps)
-    
-    _, steps = recursive_func(aloc, bloc, set(), 0)
+    _, steps = dfs(aloc, bloc, set(), 0, board)
     return steps
