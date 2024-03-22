3942ea5bf48bb4fb9e33c070f3c09b2dee0de03d
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-03-22 15:37:21 +0900

    [level 2] Title: 리코쳇 로봇, Time: 2.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -6,9 +6,10 @@ def solution(board):
     for r in range(row):
         for j in range(column):
             if board[r][j] == 'R':
-                queue.append([r, j, 0])
+                queue.append((r, j, 0))
                 break
-    visited = [[float('inf')] * column for _ in range(row)]
+    visited = set()
+    visited.add((queue[0][0], queue[0][1]))
     move_row = [-1, 1, 0, 0]
     move_column = [0, 0, -1, 1]
     while queue:
@@ -21,7 +22,9 @@ def solution(board):
             while 0 <= new_row + move_row[n] < row and 0 <= new_column + move_column[n] < column and board[new_row + move_row[n]][new_column + move_column[n]] != 'D':
                 new_row += move_row[n]
                 new_column += move_column[n]
-            if visited[new_row][new_column] > move + 1:
-                queue.append([new_row, new_column, move + 1])
-                visited[new_row][new_column] = move + 1
+            if (new_row, new_column) not in visited:
+                queue.append((new_row, new_column, move + 1))
+                visited.add((new_row, new_column))
+            else:
+                continue
     return -1
