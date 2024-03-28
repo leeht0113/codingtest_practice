97420e2f75b9b63531da564b90d475689d2df785
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-03-28 18:23:23 +0900

    [level 2] Title: 리코쳇 로봇, Time: 1.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -2,34 +2,24 @@ from collections import deque
 
 def solution(board):
     queue = deque()
-    row, column = len(board), len(board[0])
-    # "R" 로봇의 처음 위치를 찾아서 queue에 저장
-    for r in range(row):
-        for j in range(column):
-            if board[r][j] == 'R':
-                queue.append((r, j, 0))
-                break
-    visited = set()
-    # "R" 위치에서 시작
-    visited.add((queue[0][0], queue[0][1]))
-    move_row = [-1, 1, 0, 0]
-    move_column = [0, 0, -1, 1]
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 'R':
+                queue.append((i, j, 0))
+    visited = set((queue[0][0], queue[0][1]))
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     while queue:
-        r, c, move = queue.popleft()
-        # 큐에서 나온 좌표가 board의 'G'라면 이동횟수를 반환
-        if board[r][c] == 'G':
+        x, y, move = queue.popleft()
+        if board[x][y] == 'G':
             return move
-        # 상하좌우로 움직임
-        for n in range(4):
-            new_row = r
-            new_column = c
-            # "D"를 만나기 전까지 같은 방향으로 움직임
-            while 0 <= new_row + move_row[n] < row and 0 <= new_column + move_column[n] < column and board[new_row + move_row[n]][new_column + move_column[n]] != 'D':
-                new_row += move_row[n]
-                new_column += move_column[n]
-            if (new_row, new_column) not in visited:
-                # 멈춘 위치가 방문한 적이 없다면 queue에 추가, 이동횟수도 추가
-                queue.append((new_row, new_column, move + 1))
-                # 방문 set에 멈춘 위치 추가
-                visited.add((new_row, new_column))
+        for i in range(4):
+            new_x = x
+            new_y = y
+            while 0 <= new_x + dx[i] < len(board) and 0<= new_y + dy[i] < len(board[0]) and board[new_x + dx[i]][new_y + dy[i]] != 'D':
+                new_x += dx[i]
+                new_y += dy[i]
+            if (new_x, new_y) not in visited:
+                queue.append((new_x, new_y, move + 1))
+                visited.add((new_x, new_y))
     return -1
