7d8ef87407518d2876d2f692a75941efeb5c070b
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-03-31 18:09:19 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.31 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -0,0 +1,27 @@
+cnt = 0
+
+def dfs(v, visited, graph):
+    global cnt
+    visited[v] = 1
+    cnt += 1
+    for i in graph[v]:
+        if visited[i] == 0:
+            dfs(i, visited, graph)
+    # print(v, visited)
+    
+def solution(n, wires):
+    answer = float('inf')
+    graph = [[] for _ in range(n + 1)]
+    for a, b in wires:
+        graph[a].append(b)
+        graph[b].append(a)
+    # print(graph)
+    for a, b in wires:
+        global cnt
+        cnt = 0
+        visited = [0] * (n + 1)
+        visited[b] = 1
+        dfs(a, visited, graph)
+        # print(a, visited, cnt, abs((n - cnt) - (cnt)))
+        answer = min(answer, abs(n - 2 * cnt))
+    return answer
