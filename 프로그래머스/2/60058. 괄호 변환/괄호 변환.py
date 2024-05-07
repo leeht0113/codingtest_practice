a1a78602bb3754b389968b0e4c5acc1ece1592c3
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-05-07 11:34:07 +0900

    [level 2] Title: 괄호 변환, Time: 0.35 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,53 @@
+def check(p):
+    stack = []
+    for x in p:
+        if x == '(':
+            stack.append('(')
+        else:
+            if len(stack) == 0:
+                return False
+            elif stack[0] == '(':
+                stack.pop()
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+    
+def balance(p):
+    left_b = 0
+    right_b = 0
+    for idx, i in enumerate(p):
+        if i == "(":
+            left_b += 1
+        elif i == ")":
+            right_b += 1
+        if left_b == right_b:
+            u = p[:idx + 1]
+            v = p[idx + 1:]
+            return u, v
+
+def reverse(u):
+    temp = ''
+    u = u[1:-1]
+    for i in u:
+        if i =="(":
+            temp += ")"
+        elif i == ")":
+            temp += '('
+    return temp
+        
+def solution(p):
+    answer = ''
+    if len(p) == 0:
+        return ''
+    if check(p):
+        return p
+    u, v = balance(p)
+    # print(u, v)
+    if check(u):
+        return u + solution(v)
+    else:
+        temp = '(' + solution(v) + ')'
+        temp += reverse(u)
+        return temp
+    return answer
