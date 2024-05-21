861b567b16a977bdd812b36746999876d0416608
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-05-21 12:19:07 +0900

    [level 2] Title: [3차] 압축, Time: 0.64 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -6,32 +6,25 @@ def solution(msg):
     for idx, w in enumerate(word_list):
         word_dict[w] = idx + 1
     index = 26
-    # print(word_dict)
     temp = ''
     i = 0
     while i <= len(msg) - 1:
         temp += msg[i]
         # 현재 단어가 사전에 등록되어 있으면
-#         print(temp)
         if word_dict.get(msg[i], 0) != 0:
             # 다음 단어가 사전이 등록될 때까지 탐색
-#             print(msg[i + 1:])
             if len(msg[i + 1:]) > 0:
                 for m in msg[i + 1:]:
                     temp += m
-#                     print(temp)
                     if word_dict.get(temp,  0) == 0:
                         # 사전에 등록되어 있지 않다면 사전에 등록
                         index += 1
                         word_dict[temp] = index
-#                         print('----------')
-#                         print(temp)
-#                         print('----------')
                         # 이전 단어의 색인 번호 출력
                         answer.append(word_dict[temp[:-1]])
                         i += len(temp[:-1])
                         break
-                    # 추가된 단어가 사전에 등록 되어 있고 마지막 단어이면 answer에 추가
+                    # 추가된 단어가 사전에 등록 되어 있다면 answer에 추가
                     else:
                         if i + len(temp) >= len(msg):
                             answer.append(word_dict[temp])
@@ -40,7 +33,5 @@ def solution(msg):
             else:
                 answer.append(word_dict[temp])
                 i += 1
-#         print(i)
-#         print(answer)
         temp = ''
     return answer
