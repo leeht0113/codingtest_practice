f693f587b38cbea885c8b5d0cc256c1d22fad29b
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-05-28 16:07:57 +0900

    [level 2] Title: [1차] 캐시, Time: 0.73 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def solution(cacheSize, cities):
+    answer = 0
+    temp = deque()
+    if cacheSize == 0:
+        return len(cities) * 5
+    else:
+        for c in cities:
+            c = c.lower()
+            # 참조리스트에 있으면, 실행시간 1 추가
+            if c in temp:
+                answer += 1
+                temp.remove(c)
+            # 참조리스트에 없으면, 실행시간 5 추가
+            else:
+                answer += 5
+                if len(temp) >= cacheSize:
+                    temp.popleft()
+            temp.append(c)
+    return answer
