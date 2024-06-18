45bfa778dc310261a902454e8958efd275140439
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-06-18 14:58:18 +0900

    [level 3] Title: 단속카메라, Time: 0.99 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -3,9 +3,7 @@ def solution(routes):
     routes.sort(key=lambda x: x[1])
     camera = routes[0][1]
     for i in range(1, len(routes)):
-        if routes[i][0] <= camera:
-            continue
-        else:
+        if routes[i][0] > camera:
             camera = routes[i][1]
             answer += 1
     return answer
