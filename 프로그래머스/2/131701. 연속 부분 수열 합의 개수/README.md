3fc4493c3ba911d312d6018eb0e3ed80df335be6
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-06-18 12:03:22 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 291.19 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
 
 ### 성능 요약
 
-메모리: 44.1 MB, 시간: 648.48 ms
+메모리: 43.6 MB, 시간: 291.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 18일 11:52:33
+2024년 06월 18일 12:03:26
 
 ### 문제 설명
 
--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,20 +1,10 @@
 def solution(elements):
+    l = len(elements)
     answer = set()
-    elements = elements * 2
-    # print(elements)
-    sum_value = 0
-    prefix_sum = [0]
-    for e in elements:
-        sum_value += e
-        prefix_sum.append(sum_value)
-    # print(prefix_sum)
-    for i in range(1, (len(prefix_sum)//2)+ 1):
-        temp = []
-        for j in range(1, len(elements) + 1):
-            if i + j <= len(elements) - 1:
-                # print(i, j)
-                temp.append(prefix_sum[i + j] - prefix_sum[j])
-        # print(set(temp))
-        answer.update(set(temp))
-    # print(answer)
+    for i in range(l):
+        temp = elements[i]
+        answer.add(temp)
+        for j in range(i + 1, i + l):
+            temp += elements[j%l]
+            answer.add(temp)
     return len(answer)
