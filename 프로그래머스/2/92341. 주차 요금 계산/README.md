9c0ecdae350f45c95f707c7911eddc4f018bacc8
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-06-25 17:48:14 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/README.md
+++ 프로그래머스/2/92341. 주차 요금 계산/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주차 요금 계산 - 92341 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 10.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 25일 17:47:24
+2024년 06월 25일 17:48:21
 
 ### 문제 설명
 
--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import math
 
 def solution(fees, records):
@@ -6,34 +7,25 @@ def solution(fees, records):
     fee = fees[1]
     extra_time = fees[2]
     extra_fee = fees[3]
-    info_dict = {} # 누적 주차 시간 
-    info_in = {} # 마지막 입차 시간
-    last_time = 60 * 23 + 59
+    intime_dict = {}
+    in_dict = defaultdict(int)
+    sum_dict = defaultdict(int)
+    last_time = 23 * 60 + 59
     for r in records:
-        time, car_number, detail = r.split()
+        time, number, detail = r.split()
         time = list(map(int, time.split(':')))
-        time = (60 * time[0]) + (time[1])
-        # print(time)
-        if detail == 'IN':
-            info_in[car_number] = time
-            if car_number not in info_dict: # 새롭게 입차된 차량이면 info_dict에 추가
-                info_dict[car_number] = 0
-            if len(records) == 1:
-                info_dict[car_number] = (last_time - list(info_in.values())[0])
-                info_in[car_number] = -1 # 출차 처리가 됐으므로 입차 -1로 처리
-                break
+        time = time[0] * 60 + time[1]
+        if detail == "IN":
+            intime_dict[number] = time
+            in_dict[number] = 1
         else:
-            info_dict[car_number] += (time - info_in[car_number])
-            info_in[car_number] = -1 # 출차 처리가 됐으므로 입차 -1로 처리
-    for i in info_in:
-        if info_in[i] != -1: # 입차 차량 다음에 출차 차량이 없음
-            info_dict[i] += (last_time - info_in[i])
-    final_dict = {} # 최종 주차 요금
-    car_number = sorted(list(info_dict.keys()))
-    for c in car_number:
-        if info_dict[c] > f_time:
-            # 기본요금 + ((누적주차 시간 - 기본 시간)/단위시간) * 단위 요금
-            final_dict[c] = (fee + (math.ceil(((info_dict[c] - f_time)/extra_time)) * extra_fee))
-        else:
-            final_dict[c] = fee
-    return list(final_dict.values())
+            sum_dict[number] += (time - intime_dict[number])
+            in_dict[number] = 0
+    for i in in_dict:
+        if in_dict[i] == 1:
+            sum_dict[i] += (last_time - intime_dict[i])
+    car = list(sorted(sum_dict.keys()))
+    for c in car:
+        cost = fee + max(math.ceil((sum_dict[c] - f_time)/extra_time), 0) * extra_fee
+        answer.append(cost)
+    return answer
