018e380cc2f3066f3693fa0aab686f47b07e72c2
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-06-25 15:44:44 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -2,12 +2,12 @@ from collections import defaultdict
 
 def solution(id_list, report, k):
     answer = []
+    report = list(set(report))
     report_hash = defaultdict(set)
     stoped = defaultdict(int)
     for r in report:
         a, b = r.split()
-        if b not in report_hash[a]:
-            stoped[b] += 1
+        stoped[b] += 1
         report_hash[a].add(b)
     for i in id_list:
         mail = 0
