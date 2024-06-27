11f509b3a355992230ba68315ca516a83ac5a7d9
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-06-28 02:31:47 +0900

    [level 3] Title: 등굣길, Time: 2.35 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,24 +1,10 @@
-def dfs(y, x, dp, puddles):
-    row, col = len(dp), len(dp[0])
-    path = [[1, 0], [0, 1]]
-    
-    if [y, x] == [row - 1, col - 1]:
-        return 1
-    
-    if dp[y][x] != 0:
-        return dp[y][x]
-    
-    for i in range(2):
-        ny = y + path[i][0]
-        nx = x + path[i][1]
-        
-        if 0 <= ny < row and 0 <= nx < col:
-            if [nx + 1, ny + 1] in puddles:
-                continue
-            dp[y][x] += dfs(ny, nx, dp, puddles)
-            
-    return dp[y][x] % 1000000007
-
 def solution(m, n, puddles):
-    dp = [[0] * m for _ in range(n)]
-    return dfs(0, 0, dp, puddles)
+    dp =[[0] * m for _ in range(n)]
+    dp[0][0] = 1
+    
+    for y in range(n): # 행
+        for x in range(m): # 열
+            if ([x + 1, y + 1] in puddles) or ((y, x) == (0, 0)):
+                continue
+            dp[y][x] = (dp[y - 1][x] + dp[y][x - 1]) % 1000000007
+    return dp[-1][-1]
