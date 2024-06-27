21bf09890a046bdd755485b8fd3ce54c7ae0fd9e
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-06-27 18:32:16 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,11 +1,14 @@
+answer = 0
+
 def dfs(k, cnt, dungeons, visited):
-    answer = cnt
+    global answer
+    answer = max(answer, cnt)
     if answer == len(dungeons):
         return answer
     for i in range(len(dungeons)):
         if k >= dungeons[i][0] and visited[i] == 0:
             visited[i] = 1
-            answer = max(answer, dfs(k - dungeons[i][1], cnt + 1, dungeons, visited))
+            dfs(k - dungeons[i][1], cnt + 1, dungeons, visited)
             visited[i] = 0
     return answer
 
