6a18e9c4a53e1365c48f05418dc5570cfc5c9d23
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-06-28 00:14:43 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,38 +1,42 @@
 from collections import deque
 
 def solution(board):
-    def is_valid(x, y):
-        return 0 <= x < N and 0 <= y < N
+    size = len(board)
+    # 상/좌/하/우
+    paths = [(-1, 0), (0, -1), (1, 0), (0, 1)]
     
-    def is_blocked(x, y):
-        return not is_valid(x, y) or board[x][y] == 1
+    # BFS 함수를 구현
+    # BFS 함수가 여러 번 실행되어야 하므로 따로 함수를 분리하여 만듦
     
-    def calculate_cost(direction, prev_direction, cost):
-        if prev_direction == -1 or prev_direction == direction:
-            return cost + 100
-        else:
-            return cost + 600
-    
-    N = len(board)
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    visited = [[[float('inf')] * 4 for _ in range(N)] for _ in range(N)]
-    queue = deque([(0, 0, -1, 0)])  # (x, y, direction, cost)
-    
-    for i in range(4):
-        visited[0][0][i] = 0
-    
-    while queue:
-        x, y, prev_direction, cost = queue.popleft()
+    def bfs(x, y, cost, path):
+        # 방문 배열
+        graph = [[0] * size for _ in range(size)]
+        for a in range(size):
+            for b in range(size):
+                if board[a][b] == 1: graph[a][b] = -1
+                
+        q = deque()
+        q.append((x, y, cost, path))
         
-        for direction, (dx, dy) in enumerate(directions):
-            new_x, new_y = x + dx, y + dy
+        while q:
+            x, y, cost, path = q.popleft()
             
-            if is_blocked(new_x, new_y):
-                continue
-            
-            new_cost = calculate_cost(direction, prev_direction, cost)
-            
-            if visited[new_x][new_y][direction] > new_cost:
-                visited[new_x][new_y][direction] = new_cost
-                queue.append((new_x, new_y, direction, new_cost))
-    return min(visited[N - 1][N - 1])
+            for i in range(len(paths)):
+                nx = x + paths[i][0]
+                ny = y + paths[i][1]
+                
+                if nx < 0 or nx >= size or ny < 0 or ny >= size or graph[nx][ny] == -1:
+                    continue
+                # 직선도로이면 100원 추가
+                if path == i:
+                    newcost = cost + 100
+                # 코너이면 500원 추가
+                else:
+                    newcost = cost + 600
+                # 도면의 상태가 비어 있거나 방문 배열의 값이 새로운 값보다 크면 큐에 데이터를 추가
+                if graph[nx][ny] == 0 or (graph[nx][ny] != 0 and graph[nx][ny] > newcost):
+                    q.append((nx, ny, newcost, i))
+                    graph[nx][ny] = newcost
+        return graph[size - 1][size - 1]
+                
+    return min(bfs(0, 0, 0, 2), bfs(0, 0, 0, 3))
