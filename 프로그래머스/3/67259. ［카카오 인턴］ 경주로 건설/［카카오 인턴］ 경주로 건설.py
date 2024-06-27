7dfebae914f4bc9216bc5169b9efab6dacc504e9
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-06-28 00:11:14 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.09 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -5,22 +5,20 @@ def solution(board):
         return 0 <= x < N and 0 <= y < N
     
     def is_blocked(x, y):
-        return (x, y) == (0, 0) or not is_valid(x, y) or board[x][y] == 1
+        return not is_valid(x, y) or board[x][y] == 1
     
-    def calculate_cost(direrction, prev_direction, cost):
-        if prev_direction == -1 or (prev_direction - direction) % 2 == 0:
+    def calculate_cost(direction, prev_direction, cost):
+        if prev_direction == -1 or prev_direction == direction:
             return cost + 100
         else:
             return cost + 600
     
-    def isShouldUpdate(x, y, direction, new_cost):
-        return visited[x][y][direction] == 0 or visited[x][y][direction] > new_cost
-        
-    queue = deque([(0, 0, -1, 0)])
     N = len(board)
-    directions = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-    visited = [[[0 for _ in range(4)] for _ in range(N)] for _ in range(N)]
-    answer = float("inf")
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    visited = [[[float('inf')] * 4 for _ in range(N)] for _ in range(N)]
+    queue = deque([(0, 0, -1, 0)])  # (x, y, direction, cost)
+    for i in range(4):
+        visited[0][0][i] = 0
     
     while queue:
         x, y, prev_direction, cost = queue.popleft()
@@ -33,13 +31,7 @@ def solution(board):
             
             new_cost = calculate_cost(direction, prev_direction, cost)
             
-            if (new_x, new_y) == (N - 1, N - 1):
-                answer = min(answer, new_cost)
-            
-            elif isShouldUpdate(new_x, new_y, direction, new_cost):
-                queue.append((new_x, new_y, direction, new_cost))
+            if visited[new_x][new_y][direction] > new_cost:
                 visited[new_x][new_y][direction] = new_cost
-
-    return answer
-    
-    return answer
+                queue.append((new_x, new_y, direction, new_cost))
+    return min(visited[N - 1][N - 1])
