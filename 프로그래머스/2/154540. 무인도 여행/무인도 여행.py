61a2b8e7a48d4c39609cb32717413f10150549d6
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-06-28 12:37:50 +0900

    [level 2] Title: 무인도 여행, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -4,28 +4,28 @@ sys.setrecursionlimit(10000)
 
 def dfs(start, maps, visited):
     x, y = start[0], start[1]
+    visited[x][y] = 1
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     rows = len(maps)
     columns = len(maps[0])
-    visited[x][y] = 1
     days = int(maps[x][y])
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if 0 <= nx < rows and 0 <= ny < columns:
-            if maps[nx][ny] != "X" and visited[nx][ny] == 0:
+            if maps[nx][ny] != 'X' and visited[nx][ny] == 0:
                 days += dfs((nx, ny), maps, visited)
     return days
-            
+
 def solution(maps):
     answer = []
-    visited = [[0] * len(maps[0]) for _ in range(len(maps))]
     rows = len(maps)
     columns = len(maps[0])
+    visited = [[0] * columns for _ in range(rows)]
     for i in range(rows):
         for j in range(columns):
-            if maps[i][j] != "X" and visited[i][j] == 0:
+            if maps[i][j] != 'X' and visited[i][j] == 0:
                 answer.append(dfs((i, j), maps, visited))
     if len(answer) == 0:
         return [-1]
