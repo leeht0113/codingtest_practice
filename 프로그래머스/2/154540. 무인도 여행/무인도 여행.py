cfc74a836e0c74a74bdce27b2c9eb96a0b9a9792
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-06-28 12:56:49 +0900

    [level 2] Title: 무인도 여행, Time: 0.30 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,33 +1,36 @@
-import sys
-
-sys.setrecursionlimit(10000)
-
-def dfs(start, maps, visited):
-    x, y = start[0], start[1]
-    visited[x][y] = 1
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    rows = len(maps)
-    columns = len(maps[0])
-    days = int(maps[x][y])
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < rows and 0 <= ny < columns:
-            if maps[nx][ny] != 'X' and visited[nx][ny] == 0:
-                days += dfs((nx, ny), maps, visited)
-    return days
+from collections import deque
 
 def solution(maps):
     answer = []
     rows = len(maps)
     columns = len(maps[0])
     visited = [[0] * columns for _ in range(rows)]
+    
+    def bfs(start):
+        queue = deque([(start[0], start[1])])
+        visited[start[0]][start[1]] = 1
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
+        days = int(maps[start[0]][start[1]])
+        while queue:
+            x, y = queue.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < rows and 0 <= ny < columns:
+                    if maps[nx][ny] != 'X' and visited[nx][ny] == 0:
+                        queue.append((nx, ny))
+                        visited[nx][ny] = 1
+                        days += int(maps[nx][ny])
+        return days 
+    
     for i in range(rows):
         for j in range(columns):
-            if maps[i][j] != 'X' and visited[i][j] == 0:
-                answer.append(dfs((i, j), maps, visited))
+            if maps[i][j] != "X" and visited[i][j] == 0:
+                answer.append(bfs((i, j)))
+
     if len(answer) == 0:
         return [-1]
+    
     answer.sort()
     return answer
