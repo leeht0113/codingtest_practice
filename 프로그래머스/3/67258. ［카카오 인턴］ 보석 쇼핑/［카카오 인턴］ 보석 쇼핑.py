ea3e719725659efb3d9784b16da5d5290b45afa6
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-07-02 15:38:34 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 49.16 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,16 +1,16 @@
 def solution(gems):
+    answer = []
     gems_type = len(set(gems))
     if gems_type == 1:
         return [1, 1]
-    answer = []
     start = 0
-    length = len(gems)
     shortest_length = len(gems) + 1
     gems_dict = {}
+    length = len(gems)
     for end in range(length):
         curr_end = gems[end]
         gems_dict[curr_end] = gems_dict.get(curr_end, 0) + 1
-        while start < end and len(gems_dict) == gems_type:
+        while len(gems_dict) == gems_type:
             curr_length = (end - start) + 1
             if curr_length < shortest_length:
                 shortest_length = curr_length
