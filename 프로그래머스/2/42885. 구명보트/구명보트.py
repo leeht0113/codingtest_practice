b43ec2226de403d81034b977d13a9a827fa8dae8
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-07-02 16:46:21 +0900

    [level 2] Title: 구명보트, Time: 8.34 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,15 +1,11 @@
-from collections import deque
-
 def solution(people, limit):
-    result = 0
-    deque_people = deque(sorted(people))
-
-    while deque_people:
-        left = deque_people.popleft()
-        if not deque_people:
-            return result + 1
-        right = deque_people.pop()
-        if left + right > limit:
-            deque_people.appendleft(left)
-        result += 1
-    return result
+    answer = 0
+    people.sort()
+    first = 0
+    last = len(people) - 1
+    while first <= last:
+        if people[first] + people[last] <= limit:
+            first += 1
+        last -= 1
+        answer += 1
+    return answer
