3b62f296a96defa8f2a1896fabe918a557056dbf
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-07-02 15:19:53 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 52.03 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,23 +1,23 @@
 def solution(gems):
+    gems_type = len(set(gems))
+    if gems_type == 1:
+        return [1, 1]
     answer = []
-    n = len(set(gems))
+    start = 0
     length = len(gems)
-    gems_dict = {}
     shortest_length = len(gems) + 1
-    lt = 0
-    for rt in range(length):
-        curr_rt = gems[rt]
-        gems_dict[curr_rt] = gems_dict.get(curr_rt, 0) + 1
-        while len(gems_dict) == n:
-            # print((lt, rt), gems_dict, shortest_length)
-            if (rt - lt + 1) < shortest_length:
-                # print(shortest_length)
-                shortest_length = (rt - lt + 1)
-                answer = [lt + 1, rt + 1]
-            curr_left = gems[lt]
-            if gems_dict[curr_left] == 1:
-                del gems_dict[curr_left]
-            else:
-                gems_dict[curr_left] -= 1
-            lt += 1
+    gems_dict = {}
+    for end in range(length):
+        curr_end = gems[end]
+        gems_dict[curr_end] = gems_dict.get(curr_end, 0) + 1
+        while start < end and len(gems_dict) == gems_type:
+            curr_length = (end - start) + 1
+            if curr_length < shortest_length:
+                shortest_length = curr_length
+                answer = [start + 1, end + 1]
+            curr_start = gems[start]
+            gems_dict[curr_start] -= 1
+            if gems_dict[curr_start] == 0:
+                del gems_dict[curr_start]
+            start += 1
     return answer
