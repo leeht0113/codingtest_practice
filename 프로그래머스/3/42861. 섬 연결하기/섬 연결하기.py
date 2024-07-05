bbbbf11fef3e6d5a13a393dd95cf233b38176a55
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-07-05 12:07:19 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -13,10 +13,11 @@ def union_parent(parent, a, b):
 
 def solution(n, costs):
     answer = 0
+    cnt = 0
     edges = []
-    parent = [0] * (n + 1)
+    parent = [0] * n
     
-    for i in range(1, n + 1):
+    for i in range(1, n):
         parent[i] = i
         
     for c in costs:
@@ -24,11 +25,14 @@ def solution(n, costs):
         edges.append((cost, a, b))
         
     edges.sort()
-    
+
     for edge in edges:
         cost, a, b = edge
         if find_parent(parent, a) != find_parent(parent, b):
             union_parent(parent, a, b)
             answer += cost
+            cnt += 1
+        if cnt == (n - 1):
+            break
             
     return answer
