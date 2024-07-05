61ef3501fe8ac4ffec1194c599110bcbb911589a
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-07-05 11:59:24 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,34 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+def solution(n, costs):
+    answer = 0
+    edges = []
+    parent = [0] * (n + 1)
+    
+    for i in range(1, n + 1):
+        parent[i] = i
+        
+    for c in costs:
+        a, b, cost = c
+        edges.append((cost, a, b))
+        
+    edges.sort()
+    
+    for edge in edges:
+        cost, a, b = edge
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            answer += cost
+            
+    return answer
