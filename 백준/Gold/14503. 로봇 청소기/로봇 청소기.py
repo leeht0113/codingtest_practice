317668b8c7ca00b53fb4cbb91b0ba6eff00963e5
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-07-18 12:00:13 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,33 +1,30 @@
 n, m = map(int, input().split())
 r, c, d = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-
 answer = 0
-# 북, 동, 남, 서
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
-
 while True:
     if graph[r][c] == 0:
         graph[r][c] = 2
         answer += 1
     flag = False
     for i in range(1, 5):
-        nr = r + dr[d - i]
-        nc = c + dc[d - i]
+        new_d = (d - i + 4) % 4
+        nr = r + dr[new_d]
+        nc = c + dc[new_d]
         if 0 <= nr < n and 0 <= nc < m and graph[nr][nc] == 0:
             flag = True
-            d = (d - i + 4) % 4
             r = nr
             c = nc
+            d = new_d
             break
     if not flag:
         nr = r - dr[d]
         nc = c - dc[d]
-        if 0 <= nr < n and 0 <= nc < m and graph[nr][nc] != 1: 
+        if 0 <= nr < n and 0 <= nc < m and graph[nr][nc] != 1:
             r = nr
             c = nc
         else:
             break
-
 print(answer)
