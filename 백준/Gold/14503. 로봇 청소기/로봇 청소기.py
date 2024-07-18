d5cdded319f787e7a452e73f29b765014af438e4
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-07-18 11:47:01 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,41 +1,33 @@
 n, m = map(int, input().split())
-y, x, d = map(int, input().split())
-map = [list(map(int, input().split())) for _ in range(n)]
-count = 0
+r, c, d = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+answer = 0
 # 북, 동, 남, 서
-dy = [-1, 0, 1, 0]
-dx = [0, 1, 0, -1]
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
 
 while True:
-    if map[y][x] == 0:
-        map[y][x] = 2
-        count += 1
+    if graph[r][c] == 0:
+        graph[r][c] = 2
+        answer += 1
     flag = False
     for i in range(1, 5):
-        ny = y + dy[d-i]
-        nx = x + dx[d-i]
-        if 0<=ny<n and 0<=nx<m:
-            if map[ny][nx] == 0:
-                # 바라보는 방향을 기준으로
-                # 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진한다. 1번으로 돌아간다
-                d = (d - i + 4) % 4
-                y = ny
-                x = nx
-                flag = True # 청소를 한 경우
-                break
-    # 4방향 모두 있지 않은 경우
-    if flag == False:
-        # 바라보는 방향을 유지한 채로 한 칸 후진할 수 있다면 한 칸 후진하고 1번으로 돌아간다.
-        # 뒤쪽 방향이 막혀있는지 확인
-        ny = y - dy[d]
-        nx = x - dx[d]
-        if 0 <= ny < n and 0<= nx < m:
-            if map[ny][nx] == 1:
-                break
-            else:
-                y = ny
-                x = nx
+        nr = r + dr[d - i]
+        nc = c + dc[d - i]
+        if 0 <= nr < n and 0 <= nc < m and graph[nr][nc] == 0:
+            flag = True
+            d = (d - i + 4) % 4
+            r = nr
+            c = nc
+            break
+    if not flag:
+        nr = r - dr[d]
+        nc = c - dc[d]
+        if 0 <= nr < n and 0 <= nc < m and graph[nr][nc] != 1: 
+            r = nr
+            c = nc
         else:
             break
 
-print(count)
+print(answer)
