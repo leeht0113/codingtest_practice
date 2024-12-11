6d5f28a012f064ab926ac2d1103ad9ba2b76712e
leeht0113 <65216445+leeht0113@users.noreply.github.com>
2024-12-11 23:17:13 +0900

    [Silver II] Title: 예산, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -5,7 +5,7 @@ m = int(input())
 start = 0
 end = max(request)
 result = 0
-while(start <= end):
+while start <= end:
     mid = (start + end) // 2
     temp = [r if r <= mid else mid for r in request]
     total = sum(temp)
